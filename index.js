03ee5085f8ad794460f0deaceeff5c6ba9bdc1fa
sk-2049 <93445035+sk-2049@users.noreply.github.com>
2021-10-31 00:33:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,3 +42,31 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(platforms)
     }
   }
+  function movePlatforms() {
+    if (doodlerBottomSpace > 200) {
+        platforms.forEach(platform => {
+          platform.bottom -= 4
+          let visual = platform.visual
+          visual.style.bottom = platform.bottom + 'px'
+
+          if(platform.bottom < 10) {
+            let firstPlatform = platforms[0].visual
+            firstPlatform.classList.remove('platform')
+            platforms.shift()
+            console.log(platforms)
+            score++
+            var newPlatform = new Platform(600)
+            platforms.push(newPlatform)
+          }
+      }) 
+    }
+    
+  }
+
+  function createDoodler() {
+    grid.appendChild(doodler)
+    doodler.classList.add('doodler')
+    doodlerLeftSpace = platforms[0].left
+    doodler.style.left = doodlerLeftSpace + 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+  }
