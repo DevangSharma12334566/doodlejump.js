9f702e705559d2c5df07a164c9f2850374d39bbd
sk-2049 <93445035+sk-2049@users.noreply.github.com>
2021-10-31 00:35:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,3 +70,122 @@ document.addEventListener('DOMContentLoaded', () => {
     doodler.style.left = doodlerLeftSpace + 'px'
     doodler.style.bottom = doodlerBottomSpace + 'px'
   }
+
+function fall() {
+  isJumping = false
+    clearInterval(upTimerId)
+    downTimerId = setInterval(function () {
+      doodlerBottomSpace -= 5
+      doodler.style.bottom = doodlerBottomSpace + 'px'
+      if (doodlerBottomSpace <= 0) {
+        gameOver()
+      }
+      platforms.forEach(platform => {
+        if (
+          (doodlerBottomSpace >= platform.bottom) &&
+          (doodlerBottomSpace <= (platform.bottom + 15)) &&
+          ((doodlerLeftSpace + 60) >= platform.left) && 
+          (doodlerLeftSpace <= (platform.left + 85)) &&
+          !isJumping
+          ) {
+            console.log('tick')
+            startPoint = doodlerBottomSpace
+            jump()
+            console.log('start', startPoint)
+            isJumping = true
+          }
+      })
+
+    },20)
+}
+
+  function jump() {
+    clearInterval(downTimerId)
+    isJumping = true
+    upTimerId = setInterval(function () {
+      console.log(startPoint)
+      console.log('1', doodlerBottomSpace)
+      doodlerBottomSpace += 20
+      doodler.style.bottom = doodlerBottomSpace + 'px'
+      console.log('2',doodlerBottomSpace)
+      console.log('s',startPoint)
+      if (doodlerBottomSpace > (startPoint + 200)) {
+        fall()
+        isJumping = false
+      }
+    },30)
+  }
+
+  function moveLeft() {
+    if (isGoingRight) {
+        clearInterval(rightTimerId)
+        isGoingRight = false
+    }
+    isGoingLeft = true
+    leftTimerId = setInterval(function () {
+        if (doodlerLeftSpace >= 0) {
+          console.log('going left')
+          doodlerLeftSpace -=5
+           doodler.style.left = doodlerLeftSpace + 'px'
+        } else moveRight()
+    },20)
+  }
+
+  function moveRight() {
+    if (isGoingLeft) {
+        clearInterval(leftTimerId)
+        isGoingLeft = false
+    }
+    isGoingRight = true
+    rightTimerId = setInterval(function () {
+      //changed to 313 to fit doodle image
+      if (doodlerLeftSpace <= 313) {
+        console.log('going right')
+        doodlerLeftSpace +=5
+        doodler.style.left = doodlerLeftSpace + 'px'
+      } else moveLeft()
+    },20)
+  }
+  
+  function moveStraight() {
+    isGoingLeft = false
+    isGoingRight = false
+    clearInterval(leftTimerId)
+    clearInterval(rightTimerId)
+  }
+
+  //assign functions to keyCodes
+  function control(e) {
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+    if(e.key === 'ArrowLeft') {
+      moveLeft()
+    } else if (e.key === 'ArrowRight') {
+      moveRight()
+    } else if (e.key === 'ArrowUp') {
+      moveStraight()
+    }
+  }
+
+  function gameOver() {
+    isGameOver = true
+    while (grid.firstChild) {
+      console.log('remove')
+      grid.removeChild(grid.firstChild)
+    }
+    grid.innerHTML = score
+    clearInterval(upTimerId)
+    clearInterval(downTimerId)
+    clearInterval(leftTimerId)
+    clearInterval(rightTimerId)
+  }
+  function start() {
+    if (!isGameOver) {
+      createPlatforms()
+      createDoodler()
+      setInterval(movePlatforms,30)
+      jump(startPoint)
+      document.addEventListener('keyup', control)
+    } 
+  }
+  start()
+})
